90345d9e91fbee1718719ee143dc0989f437ba13
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-08-20 16:50:49 +0530

    feat: Implement auth screens and onboarding screen

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+const AuthLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+};
+
+export default AuthLayout;
